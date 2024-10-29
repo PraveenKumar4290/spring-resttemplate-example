5901493907dc607a7cb768dc0690bd1ecb78a56b
PraveenKumar4290 <praveenkumar.nadigatla@imaginnovate.com>
2024-10-29 14:32:15 +0530

    pom changed

--- src/test/java/com/imag/spring_resttemplate_example/SpringResttemplateExampleApplicationTests.java
+++ src/test/java/com/imag/spring_resttemplate_example/SpringResttemplateExampleApplicationTests.java
@@ -1,13 +1,35 @@
 package com.imag.spring_resttemplate_example;
 
+import com.imag.spring_resttemplate_example.model.request.AccountAPIClient;
+import com.imag.spring_resttemplate_example.model.request.AccountInformationDTO;
+import mockwebserver3.MockResponse;
+import mockwebserver3.MockWebServer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringResttemplateExampleApplicationTests {
 
+	@Autowired
+	private AccountAPIClient accountAPIClient;
+	private static MockWebServer mockWebServer;
+
 	@Test
-	void contextLoads() {
+	public void fetchAccountInformationTest() {
+		AccountInformationDTO accountInformationDTO = new AccountInformationDTO();
+		accountInformationDTO.setCustomerCode("GECAAK");
+		MockResponse mockResponse = new MockResponse()
+				.setResponseCode(200)
+				.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.setBody("{\"customerCode\":\"GECAAK\"}");
+		mockWebServer.enqueue(mockResponse);
+		AccountInformationDTO response = new AccountInformationDTO();
+		assertNotNull(response);
 	}
 
 }
